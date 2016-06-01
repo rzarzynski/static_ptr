445198d07e6a5ff000d9bbeb2f0bdf0517c69938
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-06-01 18:58:01 +0200

    Implement object movement between two instances of static_ptr.
    Also reflect the part of std::unique_ptr characteristic typical
    for expressing the sole ownership of a contained object as well
    as control over its lifetime.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- static_ptr.hpp
+++ static_ptr.hpp
@@ -18,28 +18,89 @@
  */
 
 #include <cstddef>
+#include <type_traits>
 
 
 template <class TypeT, size_t MaxSize>
 class static_ptr {
-  TypeT* ptr = nullptr;
+public:
+  typedef TypeT* pointer;
+
+private:
+  struct Cloneable;
+  typedef Cloneable* pointer_lcm;
+
+  /* Virtual constructors for C++... */
+  struct Cloneable {
+    virtual pointer clone_obj(void *, TypeT&&) = 0;
+    virtual pointer_lcm clone_lcm(void *) = 0;
+  };
+
   unsigned char storage[MaxSize];
+  unsigned char lcm_storage[sizeof(Cloneable)];
+  bool is_empty = true;
+
+  pointer_lcm get_lcm() noexcept {
+    return reinterpret_cast<pointer_lcm>(lcm_storage);
+  }
 
 public:
-  static_ptr() = default;
+  static_ptr() {
+  }
 
-  template <class T>
-  static_ptr(const T& t) {
-    ptr = new (storage) T(t);
+  static_ptr(static_ptr&& rhs) {
+    if (false == rhs.is_empty) {
+      rhs.get_lcm()->clone_obj(storage, std::move(*rhs.get()));
+      rhs.get_lcm()->clone_lcm(lcm_storage);
+      is_empty = false;
+    }
+  }
+
+  static_ptr& operator=(static_ptr&& rhs) {
+    if (false == rhs.is_empty) {
+      rhs.lcm->clone_obj(storage, std::move(*rhs.get()));
+      rhs.lcm->clone_lcm(lcm_storage);
+      is_empty = false;
+    }
+  }
+
+  /* Let's mimic the std::unique_ptr's behaviour. It's very useful to say to
+   * the world who controls the lifetime of the contained object. */
+  static_ptr(const static_ptr&) = delete;
+  static_ptr& operator=(const static_ptr&) = delete;
+
+
+  template <class T,
+            typename std::enable_if<std::is_base_of<TypeT, T>::value>::type* = nullptr >
+  static_ptr(T&& t) {
+    struct ClonableT : public Cloneable {
+      virtual TypeT* clone_obj(void* p, TypeT&& u) override {
+        return new (p) T(static_cast<T&&>(u));
+      }
+
+      virtual pointer_lcm clone_lcm(void* p) override {
+        return new (p) ClonableT;
+      }
+    };
+
+    new (lcm_storage) ClonableT();
+    get_lcm()->clone_obj(&storage, std::move(t));
+    is_empty = false;
   }
 
   ~static_ptr() {
-    if (ptr) {
-      ptr->~TypeT();
+    auto obj = get();
+    if (obj) {
+      obj->~TypeT();
     }
   }
 
   TypeT* operator->() {
-    return ptr;
+    return get();
   }
+
+  pointer get() noexcept {
+    return is_empty ? nullptr : reinterpret_cast<pointer>(storage);
+  }
+
 };
