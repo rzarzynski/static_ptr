853957ac46a2f7f90a137763d591680f66a1c25c
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-06-02 13:06:14 +0200

    Implement static_ptr::emplace method. Clean up the code.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- static_ptr.hpp
+++ static_ptr.hpp
@@ -48,9 +48,58 @@ private:
     return reinterpret_cast<pointer_lcm>(storage_lcm);
   }
 
-public:
-  static_ptr() {
+  template <
+    class Te,
+    class... Args,
+    /* Dummy template parameter solely for SFINAE. */
+    typename std::enable_if<
+      std::is_base_of<TypeT, Te>::value>::type* = nullptr >
+  void _emplace(Args&&... args) {
+    /* Life cycle manager for the type Te. Due to nesting this class, each
+     * instance of the emplace template will receive its own LCM with info
+     * about the concrete Te deeply buried inside. That's the way how we
+     * support "virtual constructors" and call a proper destructor even if
+     * TypeT hasn't declared its dtor as virtual. */
+    struct LCMe : public LifeCycleManager {
+      virtual void clone_obj(void* p, TypeT&& u) const override {
+        new (p) Te(static_cast<Te&&>(u));
+      }
+
+      virtual void clone_lcm(void* p) const override {
+        new (p) LCMe;
+      }
+
+      virtual void delete_obj(TypeT& t) const override {
+        static_cast<Te&>(t).~Te();
+      }
+    };
+
+    /* The storage_lcm shouldn't store anything more than the VPTR. */
+    new (storage_lcm) LCMe();
+    new (storage) Te(std::forward<Args>(args)...);
+    is_empty = false;
   }
+
+  template <class Tf, size_t Sf>
+  void _clone(static_ptr<Tf, Sf>&& rhs) {
+    typename static_ptr<Tf, Sf>::pointer rhs_obj_ptr = rhs.get();
+
+    if (rhs_obj_ptr) {
+      rhs.get_lcm()->clone_obj(storage, std::move(*rhs_obj_ptr));
+      rhs.get_lcm()->clone_lcm(storage_lcm);
+
+      /* Using the already std::moved rhs_obj_ptr is fully intensional. */
+      rhs.get_lcm()->delete_obj(*rhs_obj_ptr);
+    }
+
+    this->is_empty = rhs.is_empty;
+  }
+
+public:
+  /* All necessary things are initialized in the definitions. NOTE: we won't
+   * zeroize or touch the storage in any other way as the only thing it could
+   * bring is an impact on performance. */
+  static_ptr() noexcept = default;
 
   /* Constructor: the nullptr case. */
   static_ptr(nullptr_t) noexcept : static_ptr() {};
@@ -68,23 +117,20 @@ public:
     static_assert(std::is_base_of<TypeT, Tf>::value,
                   "constructed from non-related static_ptr instance");
 
-    if (false == rhs.is_empty) {
-      rhs.get_lcm()->clone_obj(storage, std::move(*rhs.get()));
-      rhs.get_lcm()->clone_lcm(storage_lcm);
-
-      /* Using the already std::moved rhs_obj_ptr is fully intensional. */
-      rhs.get_lcm()->delete_obj(std::move(*rhs.get()));
-    }
-
-    this->is_empty = rhs.is_empty;
+    _clone(std::move(rhs));
   }
 
-  /* Assignment: move from a compatible variant of static_ptr. Variants
-   * are compatible only if:
-   *  1) a source one is smaller or equal in terms of available storage
-   *     size than a destination one AND
-   *  2) a destination one encapsulates a type that stays in is_base_of
-   *     relationship with type stored by a source one. */
+  /* Constructor: the emplace caller. */
+  template <
+    class Tn,
+    typename std::enable_if<
+      std::is_base_of<TypeT, Tn>::value>::type* = nullptr >
+  static_ptr(Tn&& t) {
+    _emplace<Tn>(std::move(t));
+  }
+
+  /* Assignment: move from a compatible variant of static_ptr. For details
+   * please refer to the documentation of the corresponding constructor. */
   template <class Tf, size_t Sf>
   static_ptr& operator=(static_ptr<Tf, Sf>&& rhs) {
     static_assert(MaxSize >= Sf,
@@ -92,21 +138,17 @@ public:
     static_assert(std::is_base_of<TypeT, Tf>::value,
                   "assigned from non-related static_ptr instance");
 
-    auto this_obj_ptr = this->get();
+    /* First, release (destroy) the currently stored object if necessary. */
+    pointer this_obj_ptr = this->get();
     if (this_obj_ptr) {
       this->get_lcm()->delete_obj(*this_obj_ptr);
     }
 
-    auto rhs_obj_ptr = rhs.get();
-    if (rhs_obj_ptr) {
-      rhs.get_lcm()->clone_obj(storage, std::move(*rhs.get()));
-      rhs.get_lcm()->clone_lcm(storage_lcm);
+    /* Second, MoveConstruct a new object using our own storage but basing
+     * on the object hold by rhs. */
+    _clone(std::move(rhs));
 
-      /* Using the already std::moved rhs_obj_ptr is fully intensional. */
-      rhs.get_lcm()->delete_obj(*rhs_obj_ptr);
-    }
-
-    this->is_empty = rhs.is_empty;
+    return *this;
   }
 
   /* Let's mimic the std::unique_ptr's behaviour. It's very useful to say to
@@ -114,28 +156,6 @@ public:
   static_ptr(const static_ptr&) = delete;
   static_ptr& operator=(const static_ptr&) = delete;
 
-
-  template <class T,
-            typename std::enable_if<std::is_base_of<TypeT, T>::value>::type* = nullptr >
-  static_ptr(T&& t) {
-    struct ClonableT : public LifeCycleManager {
-      virtual void clone_obj(void* p, TypeT&& u) const override {
-        new (p) T(static_cast<T&&>(u));
-      }
-
-      virtual void clone_lcm(void* p) const override {
-        new (p) ClonableT;
-      }
-
-      virtual void delete_obj(TypeT& t) const override {
-        static_cast<T&>(t).~T();
-      }
-    };
-
-    new (storage_lcm) ClonableT();
-    get_lcm()->clone_obj(&storage, std::move(t));
-    is_empty = false;
-  }
 
   ~static_ptr() {
     auto obj = get();
@@ -152,4 +172,17 @@ public:
     return is_empty ? nullptr : reinterpret_cast<pointer>(storage);
   }
 
+  template <
+    class Te,
+    class... Args,
+    /* Dummy template parameter solely for SFINAE. */
+    typename std::enable_if<
+      std::is_base_of<TypeT, Te>::value>::type* = nullptr >
+  bool emplace(Args&&... args) {
+    /* The public emplace method can be called on empty static_pointer only. */
+    if (is_empty) {
+      _emplace<Te>(std::forward<Args>(args)...);
+    }
+    return is_empty;
+  }
 };
